e8d52c352ef1498b69e4d783027a8be9f3456f2c
Ujang Supriyadi <ujangsprr@gmai.com>
2023-03-27 01:31:05 +0700

    save image

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
@@ -23,14 +22,18 @@ def predict():
         results = model([img])
 
         results.render()  # updates results.imgs with boxes and labels
-        results.save(save_dir="static/image/")
+
+        img_savename = f"static/image/image0.jpg"
+        Image.fromarray(results.ims[0]).save(img_savename)
+
+        return redirect('/result')
 
     return render_template("index.html")
 
 @app.route("/result")
 def result():
     full_filename = os.path.join('static', 'image/image0.jpg')
-    return render_template("result.html", user_image = full_filename)
+    return render_template("result.html", result_image = full_filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Drowned Victim Finder")
