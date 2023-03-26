b6bb60f7996341712678581eadcc8c37d9531039
Ujang Supriyadi <ujangsprr@gmai.com>
2023-03-26 23:34:25 +0700

    firts commit

--- webapp.py
+++ webapp.py
@@ -1,7 +1,3 @@
-"""
-Simple app to upload an image via a web form 
-and view the inference results on the image in the browser.
-"""
 import argparse
 import io
 import os
@@ -27,17 +23,20 @@ def predict():
         results = model([img])
 
         results.render()  # updates results.imgs with boxes and labels
-        results.save(save_dir="static/")
-        return redirect("static/image0.jpg")
+        results.save(save_dir="static/image/")
 
     return render_template("index.html")
 
+@app.route("/result")
+def result():
+    full_filename = os.path.join('static', 'image/image0.jpg')
+    return render_template("result.html", user_image = full_filename)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser = argparse.ArgumentParser(description="Drowned Victim Finder")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
+    model = torch.hub.load('yolov5', 'custom', path='last.pt', source='local')
     model.eval()
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
